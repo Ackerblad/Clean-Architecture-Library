53fa6fcca5aa2e8c6f80560c23a3c470961bf474
Ackerblad <johanackerblad@outlook.com>
2024-11-21 19:44:36 +0100

    Implement CreateAuthorCommand and Handler with Unit Tests

---
+++ Clean-Architecture-Library/Test/AuthorTests/CommandTests/CreateAuthorCommandHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using Application.Commands.Authors.CreateAuthor;
+using Infrastructure;
+
+namespace Test.AuthorTests.CommandTests
+{
+    public class CreateAuthorCommandHandlerTests
+    {
+        private FakeDatabase _fakeDatabase;
+        private CreateAuthorCommandHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeDatabase = new FakeDatabase();
+            _handler = new CreateAuthorCommandHandler(_fakeDatabase);
+        }
+
+        [Test]
+        public async Task Handle_AuthorIsUnique_AddsAuthorToDatabase()
+        {
+            //Arrange
+            var initialAuthors = _fakeDatabase.Authors.Count;
+            var command = new CreateAuthorCommand("NewAuthor", "NewAuthor");
+
+            //Act
+            var createdAuthor = await _handler.Handle(command, CancellationToken.None);
+
+            //Assert
+            Assert.That(createdAuthor.FirstName, Is.EqualTo("NewAuthor"));
+            Assert.That(createdAuthor.LastName, Is.EqualTo("NewAuthor"));
+            Assert.That(_fakeDatabase.Authors.Count, Is.EqualTo(initialAuthors + 1));
+        }
+
+        [Test]
+        public async Task Handle_AuthorAlreadyExists_ThrowsInvalidOperationException()
+        {
+            //Arrange
+            var initialAuthors = _fakeDatabase.Authors.Count;
+            var existingAuthor = _fakeDatabase.Authors[0];
+            var command = new CreateAuthorCommand(existingAuthor.FirstName, existingAuthor.LastName);
+
+            //Act
+            Task action() => _handler.Handle(command, CancellationToken.None);
+
+            //Assert
+            Assert.ThrowsAsync<InvalidOperationException>(action);
+            Assert.That(_fakeDatabase.Authors.Count, Is.EqualTo(initialAuthors));
+        }
+    }
+}
