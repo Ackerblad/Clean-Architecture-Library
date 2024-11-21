c9dd6548abb748120163b99f3f1e040e910c3743
Ackerblad <johanackerblad@outlook.com>
2024-11-21 14:28:12 +0100

    Implement CreateBookCommand and Handler with Unit Tests

---
+++ Clean-Architecture-Library/Application/Commands/Books/CreateBook/CreateBookCommand.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Commands.Books.CreateBook
+{
+    public class CreateBookCommand : IRequest<Book>
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public int AuthorId { get; }
+
+        public CreateBookCommand(string title, string description, int authorId)
+        {
+            Title = title;
+            Description = description;
+            AuthorId = authorId;
+        }
+    }
+}
---
+++ Clean-Architecture-Library/Application/Commands/Books/CreateBook/CreateBookCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Infrastructure;
+using MediatR;
+
+namespace Application.Commands.Books.CreateBook
+{
+    public class CreateBookCommandHandler : IRequestHandler<CreateBookCommand, Book>
+    {
+        private readonly FakeDatabase _fakeDatabase;
+
+        public CreateBookCommandHandler(FakeDatabase fakeDatabase)
+        {
+            _fakeDatabase = fakeDatabase;
+        }
+
+        public Task<Book> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+        {
+            if (_fakeDatabase.Authors.All(a => a.Id != request.AuthorId))
+            {
+                throw new KeyNotFoundException($"Author with ID {request.AuthorId} does not exist.");
+            }
+
+            var newBook = new Book(
+                _fakeDatabase.Books.Count + 1,
+                request.Title,
+                request.Description,
+                request.AuthorId
+            );
+
+            _fakeDatabase.Books.Add(newBook);
+
+            return Task.FromResult(newBook);
+        }
+    }
+}
