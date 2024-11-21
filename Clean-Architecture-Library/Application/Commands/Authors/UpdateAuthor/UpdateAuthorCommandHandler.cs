9c7baf0fee133dc71ca5408e30540b8afbf3726b
Ackerblad <johanackerblad@outlook.com>
2024-11-21 20:30:04 +0100

    Implement UpdateAuthorCommand and Handler with Unit Tests

---
+++ Clean-Architecture-Library/Application/Commands/Authors/UpdateAuthor/UpdateAuthorCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Infrastructure;
+using MediatR;
+
+namespace Application.Commands.Authors.UpdateAuthor
+{
+    public class UpdateAuthorCommandHandler : IRequestHandler<UpdateAuthorCommand, Author>
+    {
+        private FakeDatabase _fakeDatabase;
+
+        public UpdateAuthorCommandHandler(FakeDatabase fakeDatabase)
+        {
+            _fakeDatabase = fakeDatabase;
+        }
+
+        public Task<Author> Handle(UpdateAuthorCommand request, CancellationToken cancellationToken)
+        {
+            var author = _fakeDatabase.Authors.FirstOrDefault(a => a.Id == request.Id);
+
+            if (author == null)
+            {
+                throw new KeyNotFoundException($"Author with ID {request.Id} not found.");
+            }
+
+            if (request.Firstname != null) author.FirstName = request.Firstname;
+            if (request.Lastname != null) author.LastName = request.Lastname;
+
+            return Task.FromResult(author);
+        }
+    }
+}
---
+++ Clean-Architecture-Library/Test/AuthorTests/CommandTests/UpdateAuthorCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using Application.Commands.Authors.UpdateAuthor;
+using Infrastructure;
+
+namespace Test.AuthorTests.CommandTests
+{
+    public class UpdateAuthorCommandHandlerTests
+    {
+        private FakeDatabase _fakeDatabase;
+        private UpdateAuthorCommandHandler _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeDatabase = new FakeDatabase();
+            _handler = new UpdateAuthorCommandHandler(_fakeDatabase);
+        }
+
+        [Test]
+        public async Task Handle_AuthorExists_UpdatesAuthor()
+        {
+            //Arrange
+            var existingAuthor = _fakeDatabase.Authors[0];
+            var command = new UpdateAuthorCommand(existingAuthor.Id, "Updated", "Author");
+
+            //Act
+            var updatedAuthor = await _handler.Handle(command, CancellationToken.None);
+
+            //Assert
+            Assert.That(updatedAuthor.FirstName, Is.EqualTo("Updated"));
+            Assert.That(updatedAuthor.LastName, Is.EqualTo("Author"));
+        }
+
+        [Test]
+        public async Task Handle_AuthorDoesNotExist_ThrowsKeyNotFoundException()
+        {
+            //Arrange
+            var command = new UpdateAuthorCommand(99, "Updated", "Author");
+
+            //Act
+            Task action() => _handler.Handle(command, CancellationToken.None);
+
+            //Assert
+            Assert.ThrowsAsync<KeyNotFoundException>(action);
+            Assert.That(_fakeDatabase.Authors.Any(a => a.Id == 99), Is.False);
+        }
+    }
+}
