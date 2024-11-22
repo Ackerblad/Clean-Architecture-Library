abe6a663e3df22b2d9cbfc022961926ccf432365
Ackerblad <johanackerblad@outlook.com>
2024-11-22 13:17:56 +0100

    Implement GetAllAuthorsQuery and Handler with Unit Tests

---
+++ Clean-Architecture-Library/Application/Queries/Authors/GetAllAuthors/GetAllAuthorsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Infrastructure;
+using MediatR;
+
+namespace Application.Queries.Authors.GetAllAuthors
+{
+    public class GetAllAuthorsQueryHandler : IRequestHandler<GetAllAuthorsQuery, List<Author>>
+    {
+        private readonly FakeDatabase _fakeDatabase;
+
+        public GetAllAuthorsQueryHandler(FakeDatabase fakeDatabase)
+        {
+            _fakeDatabase = fakeDatabase;
+        }
+
+        public Task<List<Author>> Handle(GetAllAuthorsQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_fakeDatabase.Authors);
+        }
+    }
+}
---
+++ Clean-Architecture-Library/Test/AuthorTests/QueryTests/GetAllAuthorsQueryHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using Application.Queries.Authors.GetAllAuthors;
+using Infrastructure;
+
+namespace Test.AuthorTests.QueryTests
+{
+    public class GetAllAuthorsQueryHandlerTests
+    {
+        private FakeDatabase _fakeDatabase;
+        private GetAllAuthorsQueryHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeDatabase = new FakeDatabase();
+            _handler = new GetAllAuthorsQueryHandler(_fakeDatabase);
+        }
+
+        [Test]
+        public async Task Handle_ReturnsAllAuthors()
+        {
+            //Arrange
+            var query = new GetAllAuthorsQuery();
+
+            //Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            //Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count, Is.EqualTo(_fakeDatabase.Authors.Count));
+        }
+    }
+}
