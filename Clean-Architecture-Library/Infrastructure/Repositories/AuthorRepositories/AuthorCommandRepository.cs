5d4d5305b375923f2288736ac6d6870731613464
Ackerblad <johanackerblad@outlook.com>
2024-12-04 20:04:04 +0100

    Implement repositories in infrastructure and repository interfaces in application

---
+++ Clean-Architecture-Library/Infrastructure/Repositories/AuthorRepositories/AuthorCommandRepository.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces.RepositoryInterfaces;
+using Domain.Entities;
+using Infrastructure.Database;
+
+namespace Infrastructure.Repositories.AuthorRepositories
+{
+    public class AuthorCommandRepository : ICommandRepository<Author>
+    {
+        private readonly CleanArchitectureLibraryDb _db;
+
+        public AuthorCommandRepository(CleanArchitectureLibraryDb db)
+        {
+            _db = db;
+        }
+
+        public async Task<Author> CreateAsync(Author newAuthor)
+        {
+            await _db.Authors.AddAsync(newAuthor);
+            await _db.SaveChangesAsync();
+            return newAuthor;
+        }
+
+        public async Task<Author> UpdateAsync(Author updatedAuthor)
+        {
+            _db.Authors.Update(updatedAuthor);
+            await _db.SaveChangesAsync();
+            return updatedAuthor;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var authorToDelete = await _db.Authors.FindAsync(id);
+            if (authorToDelete == null) return false;
+
+            _db.Authors.Remove(authorToDelete);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ Clean-Architecture-Library/Infrastructure/Repositories/UserRepositories/UserQueryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces.RepositoryInterfaces;
+using Domain.Entities;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.UserRepositories
+{
+    public class UserQueryRepository : IQueryRepository<User>
+    {
+        private readonly CleanArchitectureLibraryDb _db;
+
+        public UserQueryRepository(CleanArchitectureLibraryDb db)
+        {
+            _db = db;
+        }
+
+        public async Task<User> GetByIdAsync(Guid id)
+        {
+            return await _db.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _db.Users.ToListAsync();
+        }
+    }
+}
