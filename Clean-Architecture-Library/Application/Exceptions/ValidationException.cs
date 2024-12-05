9182d04ac3ca27a23a865167b0d1f4554b8d3ece
Ackerblad <johanackerblad@outlook.com>
2024-12-05 02:51:47 +0100

    Implement custom exceptions and OperationResult object

---
+++ Clean-Architecture-Library/Application/Exceptions/ValidationException.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(IEnumerable<string> errors)
+            : base($"Validation failed: {string.Join("; ", errors)}") { }
+    }
+}
